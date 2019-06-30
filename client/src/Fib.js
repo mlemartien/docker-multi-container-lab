d7b4d9452e189975431ea4356c0e0f63432aa9da
Jacques Clement <jc@MacBook-Jacques.fritz.box>
2019-06-30 14:52:44 +0200

    Overriden with course ZIP file

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -2,88 +2,76 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Fib extends Component {
-    
-    state = {
-        seenIndicies: [],
-        calculatedValues: {},
-        index: ''
-    };
+  state = {
+    seenIndexes: [],
+    values: {},
+    index: ''
+  };
 
-    componentDidMount() {
-        this.fetchValues();
-        this.fetchIndicies();
+  componentDidMount() {
+    this.fetchValues();
+    this.fetchIndexes();
+  }
+
+  async fetchValues() {
+    const values = await axios.get('/api/values/current');
+    this.setState({ values: values.data });
+  }
+
+  async fetchIndexes() {
+    const seenIndexes = await axios.get('/api/values/all');
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+
+    await axios.post('/api/values', {
+      index: this.state.index
+    });
+    this.setState({ index: '' });
+  };
+
+  renderSeenIndexes() {
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  }
+
+  renderValues() {
+    const entries = [];
+
+    for (let key in this.state.values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {this.state.values[key]}
+        </div>
+      );
     }
 
-    async fetchValues() {
-        const values = await axios.get('/api/values/current');
-        this.setState({
-            calculatedValues: values.data
-        });
-    }
+    return entries;
+  }
 
-    async fetchIndicies() {
-        const seenIndicies = await axios.get('/api/values/all');
-        this.setState({
-            seenIndicies: seenIndicies.data
-        });
-    }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Enter your index:</label>
+          <input
+            value={this.state.index}
+            onChange={event => this.setState({ index: event.target.value })}
+          />
+          <button>Submit</button>
+        </form>
 
-    handleSubmit = async (event) => {
-        /* Make sure it does not call itself */
-        event.preventDefault();
+        <h3>Indexes I have seen:</h3>
+        {this.renderSeenIndexes()}
 
-        await axios.post('/api/values', {
-            index: this.state.index
-        });
-        this.setState({index: ''});
-         
-    };
-
-    /* About the calculated values: Redis returns an object */
-
-    renderCalculatedValues() {
-        const entries = [];
-
-        for (let key in this.state.calculatedValues) {
-            entries.push(
-                <div key={key}>
-                    For index {key}, I calculated {this.state.calculatedValues[key]}
-                </div>
-            );
-        }
-
-        return entries;
-    }
-
-     /* About the seen indexes: Postgres returns by default an array of objects (hence the map() to just extract 
-       the index from the object and join all of them with a nice separator */
-
-    renderSeenIndicies() {
-        return this.state.seenIndicies.map(({ number }) => number).join(' | ');
-    }
-
-    render() {
-        return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Enter your index:</label>
-                    <input
-                        value={this.state.index}
-                        onChange={event => this.setState({ index: event.target.value })}
-                    />
-                    <button>Submit</button>
-                </form>
-
-                <h3>Indicies I have seen:</h3>
-                { this.renderSeenIndicies() }
-
-                <h3>Calculated values:</h3>
-                { this.renderCalculatedValues() }
-            </div>
-        );
-
-    }
-
-};
+        <h3>Calculated Values:</h3>
+        {this.renderValues()}
+      </div>
+    );
+  }
+}
 
 export default Fib;
